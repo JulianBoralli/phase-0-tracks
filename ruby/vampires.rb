3b41dfb94bf03b7777bb5f8338ffe54ed0c66bc5
“JulianBoralli” <jboralli@icloud.com>
2016-08-24 01:33:39 -0700

    Add assessment method and Vampire class code

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,24 +3,28 @@
 class Vampire
 	# Class variable used to store new instance information
 	@@vampires = []
-	attr_accessor :vampires
-	attr_accessor :name , :age , :year , :validate_age, :garlic_bread, :insurance
+	attr_accessor :name , :age , :year , :validate_age, :garlic_bread, :insurance, :allergies, :result
 
-	
-	# # Class method to add new vampire information
-	# def self.add_vampire(new_vampire)
-	# 	@@vampires << new_vampire
-	# end
+	def initialize
+		@allergies = []
+	end
 
-	# def self.vampires?
-	# 	@@vampires.to_s
-	# end
+	# Class method to add new vampire information to @@vampires array
+	def self.add_vampire(new_vampire)
+		@@vampires << new_vampire
+	end
+
+	# Class method that returns the @@vampires array
+	def self.vampires?
+		@@vampires
+	end
 
 end
 
-# def new_vampire
-# 	vampire = Vampire.new
-# end
+# Creates a new instance of the class Vampire.
+def new_vampire
+	vampire = Vampire.new
+end
 
 def interview (vampire)
 	# Ask for name input
@@ -60,23 +64,75 @@ def interview (vampire)
 		puts "Please answer yes or no!"
 	end
 
-	# Prints result
-	puts "Thanks #{vampire.name} for telling me you are #{vampire.age} years old, was born in #{vampire.year}."
-	puts "You also told said #{vampire.garlic_bread} and #{vampire.insurance} to insurance"
+	# Prompt for allergies
+	while true
+		puts "Please name, one by one, any allergy you are aware of:"
+		allergy = gets.chomp
+		break if allergy == "done"
+		if allergy == "sunshine"
+			vampire.result = "Probably a vampire."
+			break
+		end
+		vampire.allergies << allergy
+	end
 
 	return vampire
 end
 
-# Creates a instance of Vampire class and interviews to fill its data.
-# Then it adds the new instance to the @@vampires class variable, which 
-# is an array of all the vampire objects create.
-# Vampire.add_vampire(interview(new_vampire))
+# This method verifies the probability of a person being a vampire and returns result.
+def assessment(vampire)
+	vampire.result = "Results inconclusive."
+	if vampire.validate_age == true && (vampire.garlic_bread == "yes" || vampire.insurance == "yes")
+		vampire.result = "Probably not a vampire."
+	end
+	if vampire.validate_age == false && (vampire.garlic_bread == "no" || vampire.insurance == "no")
+		vampire.result = "Probably a vampire."
+	end
+	if vampire.validate_age == false && (vampire.garlic_bread == "no" && vampire.insurance == "no")
+		vampire.result = "Almost certainly a vampire."
+	end
+	if vampire.name == "Drake Cula" || vampire.name == "Tu Fang"
+		vampire.result = "Definitely a vampire."
+	end
+vampire
+end
 
-# Prints all the vampires in a string/array format.
-# puts Vampire.vampires?
+# This is where the program starts. It prompts the user for quantity of interviews
+while true
+	puts "How many potencial vampires do you want to interview?"
+	quantity = gets.to_i
+	break if quantity > 0
+	puts "Please provide a valida integer >= 1"
+end
 
-vampire = Vampire.new
-Vampire.vampires << interview(vampire)
-puts Vampire.vampires.to_s
+# Sets an i counter of interview to 0
+i = 0
 
+# Loops through the interview and assessment process i times
+while i < quantity do
+	puts "Let's start vampire interview number #{i + 1}"
 
+	# Creates a instance of Vampire class and interviews to fill its data.
+	vampire = interview(new_vampire)
+
+	# Assess and prints the probability of the person being a vampire and updates vampire variable
+	vampire = assessment(vampire)
+
+	# Prints result of interview and asessment
+	puts "Thanks #{vampire.name} for telling me you are #{vampire.age} years old, was born in #{vampire.year}."
+	puts "You also said #{vampire.garlic_bread} to garlic bread and #{vampire.insurance} to insurance"
+	puts "The change of the person being a vampire is: #{vampire.result}"
+	puts "These are your allergies: #{vampire.allergies.to_s}"
+
+	# Prints all the vampires in a string/array format.
+	puts Vampire.vampires?.to_s
+
+	# Adds the new instance to the @@vampires class variable, which 
+	# is an array of all the vampire objects create.
+	Vampire.add_vampire(vampire)
+
+	# Adds to the i counter for the next iteration
+	i += 1
+end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
