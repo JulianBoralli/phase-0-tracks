452b5a8b0abde7e5dc0fb271b48ba692ab913983
“JulianBoralli” <jboralli@icloud.com>
2016-09-06 14:05:36 -0700

    Add new Santa instance methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,31 @@
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@age = 0
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"] 
+	end
+
+	# Gender Setter
+	def gender=(gender)
+		@gender = gender
+	end
+
+	# Gender Getter
+	def gender
+		@gender
+	end
+
+	# Age Getter
+	def age
+		@age
+	end
+
+	# Ethnicity Getter
+	def ethnicity
+		@ethnicity
 	end
 
 	def speak
@@ -12,10 +36,55 @@ class Santa
 		puts "That was a good #{cookie}!" 
 	end
 
+	def celebrate_birthday
+		@age += 1
+	end
+
+	# Demotes a reindeer to the end of the array
+	def get_mad_at(reindeer)
+		@reindeer_ranking << @reindeer_ranking.delete(reindeer)		
+	end
+
+	# Prints information about the instance
+	def about
+		puts "Gender: #{@gender}"
+		puts "Ethnicity: #{@ethnicity}"
+		puts "Age: #{@age}"
+		p "Reindeer Ranking: #{@reindeer_ranking}"
+	end
 end
 
-santa = Santa.new
 
-santa.speak
+# Driver Code
 
-santa.eat_milk_and_cookies("maizena")
+# Testing one instance
+# santa = Santa.new("male", "eskimo")
+# santa.speak
+# santa.eat_milk_and_cookies("maizena")
+
+
+# Creating sample arrays for gender and ethinicity
+gender = ["male", "female", "other"]
+ethnicity = ["Black", "Latino", "Asian", "White"]
+
+# Santas Array
+santas = []
+
+# Creating 10 instances of Santa using randon arguments
+10.times do |i|
+	santas << Santa.new(gender[rand(gender.length)], ethnicity[rand(ethnicity.length)])
+	i.times {santas[i].celebrate_birthday}
+end
+
+# Prints information about each instance
+santas.each do |santa|
+	puts "Age is: #{santa.age}"
+	puts "Gender is: #{santa.gender}"
+	puts "Reassigning gender:"
+	santa.gender = gender[rand(gender.length)]
+	puts "New gender is: #{santa.gender}"
+	puts "--------------------------------"
+end
+
+
+
