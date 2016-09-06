4fe373512e1cdd42f3462c3c1b90505d1703a48d
“JulianBoralli” <jboralli@icloud.com>
2016-09-06 11:21:42 -0700

    Add Puppy class and instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,55 @@
 class Puppy
 
+	attr_accessor :name
+
+	# def set_name=(name)
+	# 	@name = name
+	# end
+
+	# def get_name
+	# 	@name
+	# end
+
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
+  def speak(number)
+  	number.times {puts "Woof!"}
+  end
+
+  def roll_over
+  	puts "*roll over*"
+  end
+
+  def dog_years(human_years)
+  	dog_years = 7*human_years
+  end
+
+  def jump(height)
+  	puts "#{@name} jumped #{height} feet!"
+  end
 end
 
+
+
+
+
+# Driver Code
+spot = Puppy.new
+
+spot.fetch("ball")
+
+spot.speak(3)
+
+spot.roll_over
+
+puts spot.dog_years(10)
+
+spot.name = "Spot"
+
+puts spot.name
+
+spot.jump(5)
