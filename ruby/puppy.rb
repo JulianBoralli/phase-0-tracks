e98d0e52b17fdca3a97dcf8ad014c28d0e4876b3
“JulianBoralli” <jboralli@icloud.com>
2016-09-06 10:32:58 -0700

    Design Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# fur_texture: soft
+# fur_color: varies
+# size: big
+# loudness: 8
+
+# BEHAVIOR -----------------------
+# Bites
+# Runs
+# Barks
+
+
+
+class Puppy
+end
