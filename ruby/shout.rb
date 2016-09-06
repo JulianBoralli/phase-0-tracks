48aac218ac991ffa983679f4e21743f7381ed34a
“JulianBoralli” <jboralli@icloud.com>
2016-09-06 15:38:21 -0700

    Convert Shout module to mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,17 +1,42 @@
 
-# Shout Module
+# Shout Module Standalone
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#     "YYYAAAAYYYY," + words + "!!!" + " :)"
+#   end
+# end
+
+
+# Shout Module Mixin
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
+  def yell_happily(words)
     "YYYAAAAYYYY," + words + "!!!" + " :)"
   end
 end
 
+
+class Children
+	include Shout
+end
+
+class Teacher
+	include Shout
+end
+
 # Driver Code
 
-puts Shout.yell_angrily("WTF")
+puts Children.new.yell_angrily("WTF, can't understand anything")
 
-puts Shout.yell_happily("WTF")
+puts Teacher.new.yell_happily("I got a 10")
+
+puts Teacher.new.yell_angrily("WTF, nobody has focus")
+
+puts Teacher.new.yell_happily("the bell is ringing")
