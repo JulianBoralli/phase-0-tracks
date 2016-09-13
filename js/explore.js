77bc126284898f411335e541d454410bffe31ce8
“JulianBoralli” <jboralli@icloud.com>
2016-09-13 16:56:24 -0700

    Add pseudocode

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+// Pseudocode
+
+// reverse function
+// input: string
+// output: a reversed string
+// This function cycles through each character in the string
+// in reverse order and then it adds it to a new string
+// which is returned
