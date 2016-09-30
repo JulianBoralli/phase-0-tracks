bc06567f61c4cb73ecba2dcbcc2df657741e989a
“JulianBoralli” <jboralli@icloud.com>
2016-09-29 17:56:15 -0700

    Add jquery script

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,5 +1,26 @@
+
+
 $(document).ready(function() {
 
-$("canvas").css( "border", "9px solid red" );
+for (var i=0; i < 26; i++) {
+	var letter = "<p>" + String.fromCharCode(65+i) + "<p>";
+	$( "#canvas" ).append( letter );
+	var r = Math.random() * 255;
+	r = Math.ceil(r);
+	var g = Math.random() * 255;
+	g = Math.ceil(g);
+	var b = Math.random() * 255;
+	b = Math.ceil(b);
+	var rgb = "\"rgb(" + r + "," + g + "," + b + ")\"";
+
+	console.log(rgb);
+	$( "#canvas p" ).last().prev().css( "color", rgb);
+}
+
+
+
+$( "p" ).draggable({
+    containment: '#canvas',
+  } );
 
 }); 
